2638c4cc772c8d489228584302b837b0a8d9273a
luxysiv <46205571+luxysiv@users.noreply.github.com>
2023-01-06 22:51:34 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ wget -i filters.txt
 chmod +x ruleset_converter filters.txt
 ./ruleset_converter --input_format=filter-list \
                     --output_format=unindexed-ruleset \
-                    --input_files=adguard.txt,annoyances.txt,tracking.txt,abpvn-ext.txt,spyware.txt,social_filters.txt \
+                    --input_files=2_optimized.txt \
                     --output_file=filters.dat
