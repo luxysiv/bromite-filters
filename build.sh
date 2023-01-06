c867833e8314ef168c58e0e563d7fb76daf46929
luxysiv <46205571+luxysiv@users.noreply.github.com>
2023-01-06 22:56:15 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ wget -i filters.txt
 chmod +x ruleset_converter filters.txt
 ./ruleset_converter --input_format=filter-list \
                     --output_format=unindexed-ruleset \
-                    --input_files=2_optimized.txt \
+                    --input_files=2_optimized.txt,filter.txt,17_optimized.txt,abpvn-ext.txt,filter.txt.1,filter.txt.2 \
                     --output_file=filters.dat
