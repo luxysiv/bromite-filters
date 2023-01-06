e634850ebd79a5986329d0420bf010802512252c
luxysiv <46205571+luxysiv@users.noreply.github.com>
2023-01-06 22:25:21 +0700

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir ruleset_converter
 wget https://filters.adtidy.org/android/filters/2_optimized.txt
      https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_14_Annoyances/filter.txt
      https://filters.adtidy.org/android/filters/17_optimized.txt
@@ -6,6 +7,7 @@ wget https://filters.adtidy.org/android/filters/2_optimized.txt
      https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_3_Spyware/filter.txt
      https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_4_Social/filter.txt
      https://github.com/bromite/bromite/releases/latest/download/ruleset_converter
+chmod +x ruleset_converter
 ./ruleset_converter --input_format=filter-list \
                     --output_format=unindexed-ruleset \
                     --input_files=adguard.txt,annoyances.txt,tracking.txt,abpvn-ext.txt,spyware.txt,social_filters.txt \
