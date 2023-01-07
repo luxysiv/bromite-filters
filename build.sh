a49cbb8f8569e59449d4f4a70b5a0650c201a16e
luxysiv <46205571+luxysiv@users.noreply.github.com>
2023-01-08 00:34:07 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 wget -i resources
+cat *.txt > rules.txt
+wget https://github.com/bromite/bromite/releases/latest/download/ruleset_converter
 chmod +x ruleset_converter filters.txt
 ./ruleset_converter --input_format=filter-list \
                     --output_format=unindexed-ruleset \
-                    --input_files=2_optimized.txt,14_optimized.txt,17_optimized.txt,abpvn-ext.txt,3_optimized.txt,4_optimized.txt \
+                    --input_files=rules.txt \
                     --output_file=filters.dat
