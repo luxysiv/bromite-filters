8fed28cc1f40928a0cd7a4988ae5d3db365266bf
luxysiv <46205571+luxysiv@users.noreply.github.com>
2023-01-08 18:32:47 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 aria2c -i resources
-echo *.txt | xargs cat > rules.txt
+echo *.txt | xargs cat > merge-file
 aria2c https://github.com/bromite/bromite/releases/latest/download/ruleset_converter
 chmod +x ruleset_converter
 ./ruleset_converter --input_format=filter-list \
                     --output_format=unindexed-ruleset \
-                    --input_files=rules.txt \
+                    --input_files=merge-file \
                     --output_file=filters.dat
